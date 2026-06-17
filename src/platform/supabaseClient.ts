d71ec9d7ad9ac33d8762e72cd7a6add40174c84b
AlNegocioApp <todoenoro56@gmail.com>
2026-06-17 14:57:23 -0400

    Eliminar todos los archivos viejos de GitHub

--- src/platform/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
-import { HAS_BACKEND, SUPABASE_URL, SUPABASE_ANON_KEY } from "./config";
-
-export const supabase: SupabaseClient | null = HAS_BACKEND
-  ? createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-      auth: { persistSession: true, autoRefreshToken: true },
-    })
-  : null;
